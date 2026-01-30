d72e98793e8d0f3ab6e91e4cca8f2221b2f00c7b
Jelo Dimaano <jannangelodimaano@gmail.com>
2026-01-30 23:44:02 +0800

    Add shadcn

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Button variant="outline">NIGGA</Button>
+    </nav>
+  );
+};
+
+export default Navbar;
