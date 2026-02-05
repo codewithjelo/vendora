72c16ffeef989a73c032b36f65263f6578f15140
Jelo Dimaano <jannangelodimaano@gmail.com>
2026-02-05 13:13:24 +0800

    Create collection section

---
+++ src/components/home/CollectionSection.jsx
@@ -0,0 +1,11 @@
+
+
+const CollectionSection = () => {
+  return (
+    <section className="min-h-screen flex flex-col bg-blue-500">
+      
+    </section>
+  );
+};
+
+export default CollectionSection;
