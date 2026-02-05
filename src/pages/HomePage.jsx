0757737218e2d8fbec6ce35d2834bfb592a2c929
Jelo Dimaano <jannangelodimaano@gmail.com>
2026-02-05 13:14:21 +0800

    Create home page and add hero and collection section

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import HeroSection from "@/components/home/HeroSection";
+import CollectionSection from "@/components/home/CollectionSection";
+
+const HomePage = () => {
+    return(
+        <div>
+            <HeroSection /> 
+            <CollectionSection />
+        </div>
+    );
+}
+
+
+export default HomePage;
