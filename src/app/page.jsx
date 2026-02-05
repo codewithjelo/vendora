aa3f109415956266f83ac104011a2594a3ff8bba
Jelo Dimaano <jannangelodimaano@gmail.com>
2026-02-05 21:47:14 +0800

    Import footer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
-import Navbar from "@/components/Navbar";
+import Navbar from "@/layout/Navbar";
 import HomePage from "@/pages/HomePage";
+import Footer from "@/layout/Footer";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <HomePage />
+      <Footer />
     </div>
   );
 }
