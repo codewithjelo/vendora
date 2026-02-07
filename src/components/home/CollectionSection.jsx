0dd31170df9978c66f0dde4df0b7ee05f7068da0
Jelo Dimaano <jannangelodimaano@gmail.com>
2026-02-07 20:49:14 +0800

    Adjust layout for collection and for mobile design

--- src/components/home/CollectionSection.jsx
+++ src/components/home/CollectionSection.jsx
@@ -13,99 +13,42 @@ import { Separator } from "@/components/ui/separator";
 
 const CollectionSection = () => {
   return (
-    <section className="min-h-screen py-20 xl:px-20">
-      <div className="grid grid-cols-1 gap-20">
-        <h1 className="text-4xl font-bold">FEATURED COLLECTIONS</h1>
+    <section className="md:min-h-screen py-4 px-4 xl:py-20 xl:px-20">
+      <div className="grid grid-cols-1 gap-5 md:gap-20">
+        <h1 className="text-2xl md:text-4xl font-bold">FEATURED COLLECTIONS</h1>
 
-        <div className="flex flex-row justify-evenly">
-          <Card className="relative w-full max-w-sm pt-0 w-80">
-            <div className="overflow-hidden rounded-t-xl">
-              <img
-                src="https://m.media-amazon.com/images/I/71ubHqDJexL._AC_SL1500_.jpg"
-                alt="Event cover"
-                className="relative z-20 aspect-[1/1] h-64 w-full object-fill brightness-60 grayscale dark:brightness-40 rounded-t-xl transition-transform duration-500 hover:scale-110"
-              />
-            </div>
-            <CardHeader>
-              <CardAction>
-                <Badge variant="secondary">Featured</Badge>
-              </CardAction>
-              <CardTitle>Design systems meetup</CardTitle>
-              <CardDescription>
-                A practical talk on component APIs, accessibility, and shipping
-                faster.
-              </CardDescription>
-            </CardHeader>
-            <CardFooter>
-              <Button className="w-full">View Event</Button>
-            </CardFooter>
-          </Card>
-          <Card className="relative w-full max-w-sm pt-0 w-80">
-            <div className="overflow-hidden rounded-t-xl">
-              <img
-                src="https://m.media-amazon.com/images/I/71ubHqDJexL._AC_SL1500_.jpg"
-                alt="Event cover"
-                className="relative z-20 aspect-[1/1] h-64 w-full object-fill brightness-60 grayscale dark:brightness-40 rounded-t-xl transition-transform duration-500 hover:scale-110"
-              />
-            </div>
-            <CardHeader>
-              <CardAction>
-                <Badge variant="secondary">Featured</Badge>
-              </CardAction>
-              <CardTitle>Design systems meetup</CardTitle>
-              <CardDescription>
-                A practical talk on component APIs, accessibility, and shipping
-                faster.
-              </CardDescription>
-            </CardHeader>
-            <CardFooter>
-              <Button className="w-full">View Event</Button>
-            </CardFooter>
-          </Card>
-          <Card className="relative w-full max-w-sm pt-0 w-80">
-            <div className="overflow-hidden rounded-t-xl">
-              <img
-                src="https://m.media-amazon.com/images/I/71ubHqDJexL._AC_SL1500_.jpg"
-                alt="Event cover"
-                className="relative z-20 aspect-[1/1] h-64 w-full object-fill brightness-60 grayscale dark:brightness-40 rounded-t-xl transition-transform duration-500 hover:scale-110"
-              />
-            </div>
-            <CardHeader>
-              <CardAction>
-                <Badge variant="secondary">Featured</Badge>
-              </CardAction>
-              <CardTitle>Design systems meetup</CardTitle>
-              <CardDescription>
-                A practical talk on component APIs, accessibility, and shipping
-                faster.
-              </CardDescription>
-            </CardHeader>
-            <CardFooter>
-              <Button className="w-full">View Event</Button>
-            </CardFooter>
-          </Card>
-          <Card className="relative w-full max-w-sm pt-0 w-80">
-            <div className="overflow-hidden rounded-t-xl">
-              <img
-                src="https://m.media-amazon.com/images/I/71ubHqDJexL._AC_SL1500_.jpg"
-                alt="Event cover"
-                className="relative z-20 aspect-[1/1] h-64 w-full object-fill brightness-60 grayscale dark:brightness-40 rounded-t-xl transition-transform duration-500 hover:scale-110"
-              />
-            </div>
-            <CardHeader>
-              <CardAction>
-                <Badge variant="secondary">Featured</Badge>
-              </CardAction>
-              <CardTitle>Design systems meetup</CardTitle>
-              <CardDescription>
-                A practical talk on component APIs, accessibility, and shipping
-                faster.
-              </CardDescription>
-            </CardHeader>
-            <CardFooter>
-              <Button className="w-full">View Event</Button>
-            </CardFooter>
-          </Card>
+        <div className="flex flex-row justify-evenly gap-4">
+          <div className="flex flex-row justify-evenly overflow-x-auto py-3 snap-x">
+            {[1, 2, 3, 4].map((item) => (
+              <Card
+                key={item}
+                className="relative h-100 min-w-[200px] flex-shrink-0 mx-10 pt-0 md:w-full md:h-full max-w-xs snap-center"
+              >
+                <div className="overflow-hidden rounded-t-xl">
+                  <img
+                    src="https://m.media-amazon.com/images/I/71ubHqDJexL._AC_SL1500_.jpg"
+                    alt="Event cover"
+                    className="aspect-square w-full object-cover brightness-60 grayscale dark:brightness-40 rounded-t-xl transition-transform duration-500 hover:scale-110"
+                  />
+                </div>
+
+                <CardHeader>
+                  <CardAction>
+                    <Badge variant="secondary">Featured</Badge>
+                  </CardAction>
+                  <CardTitle>Design systems meetup</CardTitle>
+                  <CardDescription>
+                    A practical talk on component APIs, accessibility, and
+                    shipping faster.
+                  </CardDescription>
+                </CardHeader>
+
+                <CardFooter>
+                  <Button className="w-full">View Event</Button>
+                </CardFooter>
+              </Card>
+            ))}
+          </div>
         </div>
         <a
           href="#"
@@ -118,29 +61,32 @@ const CollectionSection = () => {
           />
         </a>
 
-        <div className="flex flex-row space-x-5 mt-10">
+        <div className="flex flex-col lg:flex-row space-y-5 lg:space-x-5 mt-10">
           <div className="flex flex-col flex-1 justify-center items-center gap-2">
             <Globe size={48} />
-            <h1 className="text-5xl">15,000+</h1>
-            <p className="text-lg font-normal">HAPPY CUSTOMERS</p>
+            <h1 className="text-3xl lg:text-5xl">15,000+</h1>
+            <p className="text-md lg:text-lg font-normal">HAPPY CUSTOMERS</p>
             <span className="text-md font-extralight">
               Worldwide Delivery since 2023
             </span>
           </div>
-          <Separator orientation="vertical" />
+          <Separator className="hidden lg:flex" orientation="vertical" />
+          <Separator className="lg:hidden" />
           <div className="flex flex-col flex-1 justify-center items-center gap-2">
             <Award size={48} />
-            <h1 className="text-5xl">4.9★</h1>
-            <p className="text-lg font-normal">CUSTOMER RATING</p>
+            <h1 className="text-3xl lg:text-5xl">4.9★</h1>
+            <p className="text-md lg:text-lg font-normal">CUSTOMER RATING</p>
             <span className="text-md font-extralight">
               From 2,400+ verified reviews
             </span>
           </div>
-          <Separator orientation="vertical" />
+          <Separator className="hidden lg:flex" orientation="vertical" />
+          <Separator className="lg:hidden" />
+
           <div className="flex flex-col flex-1 justify-center items-center gap-2">
             <Stars size={48} />
-            <h1 className="text-5xl">100%</h1>
-            <p className="text-lg font-normal">SATISFACTION</p>
+            <h1 className="text-3xl lg:text-5xl">100%</h1>
+            <p className="text-md lg:text-lg font-normal">SATISFACTION</p>
             <span className="text-md font-extralight">
               Love it or we'll make it right
             </span>
