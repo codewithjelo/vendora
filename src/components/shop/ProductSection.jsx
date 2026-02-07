8458cc5b5e4d08fab1486f65af5f4182d697f275
Jelo Dimaano <jannangelodimaano@gmail.com>
2026-02-07 21:05:24 +0800

    Create section for product preview

---
+++ src/components/shop/ProductSection.jsx
@@ -0,0 +1,20 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+const ProductSection = () => {
+    return(
+        <div className="min-h-screen p-4 xl:py-20 xl:px-20 bg-gray-500">
+
+        </div>
+    );
+}
+
+export default ProductSection;
