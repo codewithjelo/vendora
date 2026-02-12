d89a68964df0614ef8b4999d0f82e0be308bf668
Jelo Dimaano <jannangelodimaano@gmail.com>
2026-02-12 20:34:00 +0800

    Wrap layout with cart provider

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,15 +1,16 @@
 import { FavoritesProvider } from "@/contexts/FavoritesContext";
-import { Toaster } from "@/components/ui/sonner"
+import { CartProvider } from "@/contexts/CartContext";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <FavoritesProvider>
-          {children}
-        </FavoritesProvider>
-        <Toaster className="absolute bottom-5 right-5 z-50" />
+        <CartProvider>
+          <FavoritesProvider>
+            {children}
+          </FavoritesProvider>
+        </CartProvider>
       </body>
     </html>
   );
