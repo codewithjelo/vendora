20a3fb277595d4ffb1a05f91de82642b722b693a
Jelo Dimaano <jannangelodimaano@gmail.com>
2026-02-21 19:03:18 +0800

    Major layout change for login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,15 @@
 "use client";
 import { useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { EyeIcon, EyeOff, FacebookIcon } from "lucide-react";
+import { FaGoogle } from "react-icons/fa";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const router = useRouter();
   const searchParams = useSearchParams();
   const redirect = searchParams.get("redirect") || "/";
@@ -13,7 +18,6 @@ export default function Login() {
     e.preventDefault();
 
     if (email === "test@test.com" && password === "123456") {
-
       localStorage.setItem("user", JSON.stringify({ name: "John" }));
 
       document.cookie = "token=loggedin; path=/; SameSite=Lax";
@@ -24,38 +28,76 @@ export default function Login() {
     }
   };
 
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
+
   return (
     <div className="flex justify-center items-center min-h-screen">
       <form
         onSubmit={handleLogin}
-        className="flex flex-col gap-2 p-6 border rounded shadow-md w-80"
+        className="flex flex-col gap-2 p-6 border rounded shadow-md w-150 h-120 mx-4 md:mx-auto"
       >
-        <h2 className="text-lg font-bold mb-2 text-center">Login</h2>
+        <h2 className="text-3xl font-bold text-center">
+          VENDORA
+        </h2>
 
         <input
           type="email"
           placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          className="border p-2 rounded"
+          className="border p-2 rounded mt-auto"
           required
         />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="border p-2 rounded"
-          required
-        />
+        <div className="flex flex-row relative">
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="flex-1 border p-2 rounded"
+            required
+          />
+          <button
+            className="absolute right-3 top-3 text-foreground"
+            type="button"
+            onClick={togglePasswordVisibility}
+          >
+            {showPassword ? <EyeOff size={16} /> : <EyeIcon size={16} />}
+          </button>
+        </div>
 
-        <button
-          type="submit"
-          className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+        <a
+          href="#"
+          className="text-sm text-right text-stone-600 hover:underline"
         >
+          Forgot Password?
+        </a>
+
+        <Button type="submit" className="mt-auto">
           Login
-        </button>
+        </Button>
+
+        <Separator className="my-4" />
+
+        <div className="flex flex-row gap-3">
+          <Button className="flex-1" variant="outline">
+            <FacebookIcon fill="text-foreground" size={16} />
+          </Button>
+          <Button className="flex-1" variant="outline">
+            <FaGoogle className="text-foreground" size={16} />
+          </Button>
+        </div>
+
+        <span className="text-center text-sm text-foreground mt-auto">
+          I don't have an account?
+          <a href="#" className="text-blue-500 hover:underline ml-1">
+            Sign Up
+          </a>
+        </span>
       </form>
+
       <span className="absolute bottom-5 right-5">
         <p className="italic text-sm text-stone-400">Email: test@test.com</p>
         <p className="italic text-sm text-stone-400">Password: 123456</p>
