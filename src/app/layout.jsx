bca66c86b305af2fc93f1eef4f0ef57bab264067
Jelo Dimaano <jannangelodimaano@gmail.com>
2026-03-03 19:31:21 +0800

    Wrap layout with AuthProvider

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { FavoritesProvider } from "@/contexts/FavoritesContext";
 import { CartProvider } from "@/contexts/CartContext";
+import { AuthProvider } from "@/contexts/AuthContext";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
@@ -7,10 +8,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <CartProvider>
-          <FavoritesProvider>{children}</FavoritesProvider>
-        </CartProvider>
-        <Toaster className="absolute bottom-5 right-5 z-50" />
+        <AuthProvider>
+          <CartProvider>
+            <FavoritesProvider>{children}</FavoritesProvider>
+          </CartProvider>
+          <Toaster className="absolute bottom-5 right-5 z-50" />
+        </AuthProvider>
       </body>
     </html>
   );
